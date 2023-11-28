8d7154da9195bd3ecf434778729a84d318a5148d
An Tran <102011433+antran28@users.noreply.github.com>
2023-11-28 13:42:29 +1100

    Update README.py

--- Day 1:  Division and Square-root/README.py
+++ Day 1:  Division and Square-root/README.py
@@ -1,5 +1,9 @@
-> *Write a function called divide_or_square that takes one argument (a number) and returns the square root of the number if it is divisible by 5, or its remainder if it is not divisible by 5. For example, if you pass 10 as an argument, then your function should return 3.16 as the square root.*
-```
+> Write a function called divide_or_square that takes one 
+argument (a number) and returns the square root of the number if 
+it is divisible by 5, or its remainder if it is not divisible by 5. For 
+example, if you pass 10 as an argument, then your function should 
+return 3.16 as the square root
+
 def divide_or_square(n):
     if n % 5 == 0:
         return print(n ** 0.5)
@@ -7,15 +11,14 @@ def divide_or_square(n):
         return print(n % 5)
 
 divide_or_square(10) #3.1622776601683795
-```
-> *Extra Challenge: Longest Value
+
+> Extra Challenge: Longest Value
 Write a function called longest_value that takes a dictionary as 
 an argument and returns the first longest value of the dictionary. 
 For example, the following dictionary should return "apple" as the 
 longest value.
-fruits = {'fruit': 'apple', 'color': 'green'}*
+fruits = {'fruit': 'apple', 'color': 'green'}
 
-```
 def longest_value(input_dict):
     # Check if the dictionary is not empty
     if input_dict:
@@ -29,4 +32,4 @@ def longest_value(input_dict):
 # Example usage
 fruits = {'fruit': 'apple', 'color': 'green', 'any': 'acradabra'}
 longest_value(fruits)  #acradabra
-```
+
